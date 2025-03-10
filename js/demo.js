90b979a65a1504c491403a3ebb4ef7f4627d1e98
cpalmer-ios <write.to.chris.palmer@gmail.com>
2025-03-10 19:06:56 +0000

    Updates

--- js/demo.js
+++ js/demo.js
@@ -4,7 +4,8 @@
  * Demo.
  */
 
-import { gsap } from "gsap";
+// Removing the module imports since we'll use the global GSAP from CDN
+// import { gsap } from "gsap";
 import { TextAnimator } from './text-animator.js';
 
 var debounce=function(e,t,n){var a;return function(){var r=this,i=arguments,o=function(){a=null,n||e.apply(r,i)},s=n&&!a;clearTimeout(a),a=setTimeout(o,t||200),s&&e.apply(r,i)}};
